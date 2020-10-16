21305390e2add5b55c5e86c204964c22ec74fc8f
Nathan Phillips <phillips4100@gmail.com>
2020-10-16 13:18:51 -0600

    html and JS changes

--- static/js/app.js
+++ static/js/app.js
@@ -1,166 +1,165 @@
-
-
 function init() {
     // select dropdown menu read data and append to dropdown list
     var dropdown = d3.select("#selDataset");
-    d3.json("samples.json").then((data)=> {
-        // console.log(data)
-        data.names.forEach(function(name) {
-            dropdown.append("option").text(name).property("value");
-        });
+    d3.json("../../data/json/activities.json").then((data)=> {
+        console.log(data)
+        // data.activityId.forEach(function(activity) {
+        //     dropdown.append("option").text(activity).property("value");
+        // }
+        );
 
-    //    display the data and the plots to the page
-        getPlots(data.names[0]);
-        getDemo(data.names[0]);
-    });
-}
+//     //    display the data and the plots to the page
+//         getPlots(data.names[0]);
+//         getDemo(data.names[0]);
+//     });
+// }
 
-init();
+// init();
 
-function getPlots(id) {
-    d3.json("samples.json").then ((filter) =>{
-        var samples = filter.samples;
-        // console.log(samples)
-        var filtered = samples.filter(s => s.id.toString() === id)[0];
-        // console.log(filtered)
-        var values =  filtered.sample_values.slice(0,10).reverse();
-        var ids = filtered.otu_ids;
-        var labels =  filtered.otu_labels;
-        // get top 10 otu ids and reversing for the plot OTU. 
-        var top_ids = ( filtered.otu_ids.slice(0, 10)).reverse();
-        var otuIds = top_ids.map(d => "otu " + d);
+// function getPlots(id) {
+//     d3.json("samples.json").then ((filter) =>{
+//         var samples = filter.samples;
+//         // console.log(samples)
+//         var filtered = samples.filter(s => s.id.toString() === id)[0];
+//         // console.log(filtered)
+//         var values =  filtered.sample_values.slice(0,10).reverse();
+//         var ids = filtered.otu_ids;
+//         var labels =  filtered.otu_labels;
+//         // get top 10 otu ids and reversing for the plot OTU. 
+//         var top_ids = ( filtered.otu_ids.slice(0, 10)).reverse();
+//         var otuIds = top_ids.map(d => "otu " + d);
 
-        // build trace for Bar chart
-        var trace1 =[{
-            type: 'bar',
-            orientation: 'h',
-            x: values,
-            y: otuIds,
-            text: labels,
-        }];
+//         // build trace for Bar chart
+//         var trace1 =[{
+//             type: 'bar',
+//             orientation: 'h',
+//             x: values,
+//             y: otuIds,
+//             text: labels,
+//         }];
 
-        // Build trace for Bubble graph
-        var trace2 = [{
-            x: filtered.otu_ids.slice(0, 10),
-            y: filtered.sample_values.slice(0,10),
-            mode: "markers",
-            marker: {
-                size: filtered.sample_values.slice(0,10),
-                color: filtered.otu_ids.slice(0, 10)
-            },
-            text:  labels,
-        }];
+//         // Build trace for Bubble graph
+//         var trace2 = [{
+//             x: filtered.otu_ids.slice(0, 10),
+//             y: filtered.sample_values.slice(0,10),
+//             mode: "markers",
+//             marker: {
+//                 size: filtered.sample_values.slice(0,10),
+//                 color: filtered.otu_ids.slice(0, 10)
+//             },
+//             text:  labels,
+//         }];
 
-        // bar chart layout
-        var layout1 = {
-            title: "Top 10 OTUs",
-            width: 500
-        }
+//         // bar chart layout
+//         var layout1 = {
+//             title: "Top 10 OTUs",
+//             width: 500
+//         }
 
-        // Insert Plots
-        Plotly.newPlot('bar', trace1, layout1);
-        Plotly.newPlot('bubble', trace2);
-    });
-};
+//         // Insert Plots
+//         Plotly.newPlot('bar', trace1, layout1);
+//         Plotly.newPlot('bubble', trace2);
+//     });
+// };
 
-// Call samples for demographic data, collect washing data to variable
-function getDemo(id) {
-    d3.json("samples.json").then((data)=> {
-        var metadata = data.metadata;
+// // Call samples for demographic data, collect washing data to variable
+// function getDemo(id) {
+//     d3.json("samples.json").then((data)=> {
+//         var metadata = data.metadata;
     
-        // select demo panel filter meta data by id and append to list
-        var demographicInfo = d3.select("#sample-metadata");
-        var result = metadata.filter(meta => meta.id.toString() === id)[0];
-        demographicInfo.html("");
-        var wfreq = result.wfreq
-        Object.entries(result).forEach((key) => {   
-            demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");
+//         // select demo panel filter meta data by id and append to list
+//         var demographicInfo = d3.select("#sample-metadata");
+//         var result = metadata.filter(meta => meta.id.toString() === id)[0];
+//         demographicInfo.html("");
+//         var wfreq = result.wfreq
+//         Object.entries(result).forEach((key) => {   
+//             demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");
 
-        // console.log(wfreq);
+//         // console.log(wfreq);
 
-        // Guage: define position of guade (0 to 9) by wash frequency
-        var level = (wfreq/9) * 180;
-        var degrees = 180 - level, radius = .5;
-        var radians = degrees * Math.PI / 180;
-        var x = radius * Math.cos(radians);
-        var y = radius * Math.sin(radians);
+//         // Guage: define position of guade (0 to 9) by wash frequency
+//         var level = (wfreq/9) * 180;
+//         var degrees = 180 - level, radius = .5;
+//         var radians = degrees * Math.PI / 180;
+//         var x = radius * Math.cos(radians);
+//         var y = radius * Math.sin(radians);
 
-        var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
-            pathX = String(x),
-            space = ' ',
-            pathY = String(y),
-            pathEnd = ' Z';
-        var path = mainPath.concat(pathX, space, pathY, pathEnd);
+//         var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+//             pathX = String(x),
+//             space = ' ',
+//             pathY = String(y),
+//             pathEnd = ' Z';
+//         var path = mainPath.concat(pathX, space, pathY, pathEnd);
 
-        // build trace for guage
-        var trace3 = [{ type: 'scatter',
-        x: [0], y:[0],
-            marker: {size: 18, color:'850000'},
-            showlegend: false,
-            name: 'Frequency',
-            text: wfreq,
-            hoverinfo: 'name+:+text'},
-        { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
-        rotation: 90,
-        text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
-        textinfo: 'text',
-        textposition:'inside',
-        marker: {
-            colors: ['rgba(119, 170, 221, .5)', 
-                'rgba(153, 221, 255, .5)', 'rgba(68, 187, 153, .5)', 
-                'rgba(187, 204, 51, .5)', 'rgba(170, 170, 0, .5)', 
-                'rgba(238, 221, 136, .5)', 'rgba(238, 136, 102, .5)', 
-                'rgba(255, 170, 187, .5)', 'rgba(221, 221, 221, .5)', 
-                'rgba(255, 255, 255, 0)']},
-        labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
-        hoverinfo: 'label',
-        hole: .5,
-        type: 'pie',
-        showlegend: false
-        }];
+//         // build trace for guage
+//         var trace3 = [{ type: 'scatter',
+//         x: [0], y:[0],
+//             marker: {size: 18, color:'850000'},
+//             showlegend: false,
+//             name: 'Frequency',
+//             text: wfreq,
+//             hoverinfo: 'name+:+text'},
+//         { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
+//         rotation: 90,
+//         text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+//         textinfo: 'text',
+//         textposition:'inside',
+//         marker: {
+//             colors: ['rgba(119, 170, 221, .5)', 
+//                 'rgba(153, 221, 255, .5)', 'rgba(68, 187, 153, .5)', 
+//                 'rgba(187, 204, 51, .5)', 'rgba(170, 170, 0, .5)', 
+//                 'rgba(238, 221, 136, .5)', 'rgba(238, 136, 102, .5)', 
+//                 'rgba(255, 170, 187, .5)', 'rgba(221, 221, 221, .5)', 
+//                 'rgba(255, 255, 255, 0)']},
+//         labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+//         hoverinfo: 'label',
+//         hole: .5,
+//         type: 'pie',
+//         showlegend: false
+//         }];
 
-        var layout2 = {
-            shapes:[{
-                type: 'path',
-                path: path,
-                fillcolor: '850000',
-                line: {
-                    color: '850000'
-                }
-            }],
-        title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per Week',
-        xaxis: {
-            zeroline:false, 
-            showticklabels:false,
-            showgrid: false, 
-            range: [-1, 1]
-        },
-        yaxis: {
-            zeroline:false, 
-            showticklabels:false,
-            showgrid: false, 
-            range: [-1, 1]}
-        };
-        Plotly.newPlot("gauge", trace3, layout2);
-        });
-    });
-}
+//         var layout2 = {
+//             shapes:[{
+//                 type: 'path',
+//                 path: path,
+//                 fillcolor: '850000',
+//                 line: {
+//                     color: '850000'
+//                 }
+//             }],
+//         title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per Week',
+//         xaxis: {
+//             zeroline:false, 
+//             showticklabels:false,
+//             showgrid: false, 
+//             range: [-1, 1]
+//         },
+//         yaxis: {
+//             zeroline:false, 
+//             showticklabels:false,
+//             showgrid: false, 
+//             range: [-1, 1]}
+//         };
+//         Plotly.newPlot("gauge", trace3, layout2);
+//         });
+//     });
+// }
 
-function optionChanged(id) {
-    getPlots(id);
-    getDemo(id);
-}
+// function optionChanged(id) {
+//     getPlots(id);
+//     getDemo(id);
+// }
 
-// // Call updatePlotly() when a change takes place
-d3.selectAll("#selDataset").on("change", updatePlotly);
+// // // Call updatePlotly() when a change takes place
+// d3.selectAll("#selDataset").on("change", updatePlotly);
 
-// // This function is called when a dropdown menu item is selected
-function updatePlotly() {
-  // Use D3 to select the dropdown menu
-  var dropdownMenu = d3.select("#selDataset");
-  // Assign the value of the dropdown menu option to a variable
-  var dataset = dropdownMenu.property("value");
-}
+// // // This function is called when a dropdown menu item is selected
+// function updatePlotly() {
+//   // Use D3 to select the dropdown menu
+//   var dropdownMenu = d3.select("#selDataset");
+//   // Assign the value of the dropdown menu option to a variable
+//   var dataset = dropdownMenu.property("value");
+// }
 
 // **** Scratch code ****
 
