889f818f7274fd986326f834c454c678fdf9c8b5
Nathan Phillips <phillips4100@gmail.com>
2020-10-20 10:59:44 -0600

    app.py update plus test data

--- static/js/app.js
+++ static/js/app.js
@@ -22,7 +22,102 @@ var map = L.map("map-id", {
     zoom: 12,
   });
 
-  // Add our tile layer to the map
+// Add our tile layer to the map
 gomap.addTo(map);
 
+var svg = d3.select(map.getPanes().overlayPane).append("svg");
+var g = svg.append("g").attr("class", "leaflet-zoom-hide");
 
+// d3.json('../../data/test.geojson', function(data) {
+//   // console.log(data)
+// });
+
+// the latitude and longitude coordinates will need to be transformed
+// var transform = d3.geo.transform({
+//   point: projectPoint
+// });
+// var d3path = d3.geo.path().projection(transform);
+
+// function projectPoint(x, y) {
+//   var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+//   this.stream.point(point.x, point.y);
+// } 
+
+//   // unction to convert our points to a line 
+//   function applyLatLngToLayer(d) {
+//     var y = d.geometry.coordinates[1]
+//     var x = d.geometry.coordinates[0]
+//     return map.latLngToLayerPoint(new L.LatLng(y, x))
+// }
+
+// We have several elements we will be adding. These include the path itself (as a line), the yellow traveling circle, the points themselves
+// here is the line between points
+
+var features = []
+// data.forEach(point => {
+//   Object.entries(point).append
+// })
+// var data = 
+d3.json('../../data/test2.geojson').then(data => {
+  console.log(data.features[1].geometry.coordinates)
+});
+// var arr =   
+// var arr = L.geoJSON('../../data/test2.geojson').then(data => {
+//   return arr
+// })
+// for (data [Key, Value] of Object.defineProperties(data)) {
+//   console.log(`${key}: ${value}`);
+// }
+
+// L.geoJson(features, {
+//   filter: function(feature, layer) {
+//       return feature.properties.show_on_map;
+//   }
+// }).addTo(map);
+
+// var linePath = g.selectAll(".lineConnect")
+//     .data([featuresdata])
+//     .enter()
+//     .append("path")
+//     .attr("class", "lineConnect");
+
+// // This will be our traveling circle
+// var marker = g.append("circle")
+//     .attr("r", 10)
+//     .attr("id", "marker")
+//     .attr("class", "travelMarker");
+
+// // if you want the actual points change opacity
+// var ptFeatures = g.selectAll("circle")
+//     .data(featuresdata)
+//     .enter()
+//     .append("circle")
+//     .attr("r", 3)
+//     .attr("class", function(d){
+//         return "waypoints " + "c" + d.properties.time
+//     })      
+//     .style("opacity", 0);
+
+// // I want the origin and destination to look different
+// var originANDdestination = [featuresdata[0], featuresdata[17]]
+
+// var begend = g.selectAll(".drinks")
+//     .data(originANDdestination)
+//     .enter()
+//     .append("circle", ".drinks")
+//     .attr("r", 5)
+//     .style("fill", "red")
+//     .style("opacity", "1");
+
+//     // I want names for my coffee and beer
+// var text = g.selectAll("text")
+//     .data(originANDdestination)
+//     .enter()
+//     .append("text")
+//     .text(function(d) {
+//         return d.properties.name
+//     })
+//     .attr("class", "locnames")
+//     .attr("y", function(d) {
+//         return -10 //I'm moving the text UP 10px
+//     })
